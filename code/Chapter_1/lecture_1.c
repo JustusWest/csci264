88da5914743d2299c991d38f234dfb4159cbabae
Michael West <38738198+JustusWest@users.noreply.github.com>
2026-08-26 11:49:08 -0400

    LEcture 1 code examples

--- code/Chapter_1/lecture_1.c
+++ code/Chapter_1/lecture_1.c
@@ -2,7 +2,33 @@
 #include <math.h>
 
 int main(void) {
+	char letter;
+	letter = 'a';
+	letter = letter + 1;
+	printf("Letter: %d\n", letter);
+	
+	int y;
+	y = pow(2, 32);
+	printf("%d\n", y);
+
+	int a, b;
+	float c, d;
+
+	a = 11;
+	b = 2;
+	a++;
+	c = 11.0;
+	d = 2.0;
+
+	printf("Int division: %d\n", a/b);
+        printf("Float division: %f\n", a/d);
+        printf("Unsigned: %u\n", -110);
+
+	scanf("%d", &a);
+	printf("After scan: %d\n", a);
 
 
-    return 0;
+	 return 0;
+	
+
 }
