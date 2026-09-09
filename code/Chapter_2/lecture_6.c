2d31f8b6227cfd855ee834052aababb00db69a35
Michael West <38738198+JustusWest@users.noreply.github.com>
2026-09-09 13:34:51 -0400

    lecture 6 example code

--- code/Chapter_2/lecture_6.c
+++ code/Chapter_2/lecture_6.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int max(int x, int y){
+	int bigger;
+	if(x>y){
+		bigger = x;
+	}else{
+		bigger = y;
+	}
+	return bigger;
+}
+
+
 
 int main(void) {
 	int x, y, result;
@@ -7,8 +18,26 @@ int main(void) {
 	scanf("%d", &x);
 	printf("Enter value 2: ");
 	scanf("%d", &y);
-
 	
+	printf("x holds %d and lives at %p\n", x, &x);
+
+
+	int *ptr1, *ptr2, a, b;
+	
+	a = 8;
+	ptr2 = &a;
+	ptr1 = NULL;
+
+	*ptr2 = 10;
+	b = *ptr2 + 3;
+	
+	ptr1 = ptr2;
+	*ptr1 = 80;
+
+	ptr1 = &b;
+	*ptr1 = 100;
+
+	result = max(x, y);
 	printf("%d is the largest of %d and %d\n", result, x, y);
 	return 0;
 }
